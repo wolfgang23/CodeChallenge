e850e0b52f8d690a466fb12a92a01a2587d2bbaf
Leo <Ortiz>
2020-03-12 13:19:18 -0600

    delete function ...

--- TestRest/DataGenerator.cs
+++ TestRest/DataGenerator.cs
@@ -7,16 +7,6 @@ namespace TestRest
 {
     public class DataGenerator
     {
-        public static void AddTestData(MyDbContext context)
-        {
-            Models.Transaction t1 = new Models.Transaction(1, System.DateTime.Now, 12, "test");
-            Models.Transaction t2 = new Models.Transaction(2, System.DateTime.Now, 134, "test2");
-            Models.Transaction t3 = new Models.Transaction(3, System.DateTime.Now, 6556, "test3");
-            context.Add(t1);
-            context.Add(t2);
-            context.Add(t3);
-
-            context.SaveChanges();
-        }
+        
     }
 }
